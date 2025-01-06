825bb8fa082c26b4d352bcdd5e2f8c1c8539ef39
ACM UCR <fake@domain.com>
2025-01-06 01:17:24 +0000

    convert images to webp

--- src/components/live/footer.tsx
+++ src/components/live/footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "@/public/logos/short.png";
+import logo from "@/public/logos/short.webp";
 import Link from "next/link";
 import { Mail } from "lucide-react";
 import {
