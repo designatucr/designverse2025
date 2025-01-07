f8a23186c3e14d6e0d624949cedb20bcee7bf443
Stanley Lew <stanley43595@gmail.com>
2025-01-06 17:05:56 -0800

    add config vars

--- src/data/config.ts
+++ src/data/config.ts
@@ -19,16 +19,17 @@ const data: config = {
   name: "DesignVerse 2025",
   short_name: "DesignVerse",
   email: "designverseucr@gmail.com",
-  description: "DESCRIPTION",
+  description:
+    "DesignVerse is UCR’s UI/UX Design-a-Thon! Over the course of 24 hours, you’ll be able to challenge your problem solving skills, create innovative prototypes, and engage with a panel of industry experts!",
   length: 24,
   date: new Date("2025-05-11T08:00:00"),
   end: new Date("2025-05-12T16:00:00"),
   packet: "",
-  devpost: "https://devpost.com/",
-  domain: "https://www.placeholder.com",
-  instagram: "https://www.instagram.com",
-  linkedin: "https://www.linkedin.com",
-  discord: "https://www.discord.com",
+  devpost: "https://designverseucr.devpost.com",
+  domain: "https://www.designverseucr.com",
+  instagram: "https://www.instagram.com/designverseucr/",
+  linkedin: "https://www.linkedin.com/company/designverseucr",
+  discord: "https://discord.gg/Vck5zFjz6m",
   heart: "💙",
 };
 
