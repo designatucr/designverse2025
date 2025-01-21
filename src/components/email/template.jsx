e0e73cbfee8ad30bcd05478b6639d953a0d96fa3
shahdivyank <divyank.shah.2016@gmail.com>
2025-01-20 23:29:48 -0800

    update email

--- src/components/email/template.jsx
+++ src/components/email/template.jsx
@@ -24,7 +24,7 @@ const Template = ({ children, name, preview }) => {
           <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] bg-[#456B7F] p-[20px]">
             <Section className="mt-[32px]">
               <Img
-                src="https://www.bearhack.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.473367c8.webp&w=3840&q=75"
+                src="https://www.designverseucr.org/_next/static/media/primary.4ebfacac.svg"
                 width="40"
                 height="37"
                 alt="DesignVerse"
