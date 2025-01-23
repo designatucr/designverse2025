b852708f83f80a9260a1a0da95c998c1fbe5ce7c
py1u.dev <py1u.dev@gmail.com>
2025-01-23 15:19:10 -0800

    style: adjustments to card padding and typography

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -10,42 +10,52 @@ const tracks = [
     title: "Atlantis Adventure",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Netus porttitor sem",
+    width: 40,
+    height: 100,
   },
   {
     image: Trident,
     title: "Visionary Voyage",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Netus porttitor sem",
+    width: 120,
+    height: 100,
   },
   {
     image: Shells,
     title: "Waves of Change",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Netus porttitor sem",
+    width: 160,
+    height: 100,
   },
   {
     image: Coral,
     title: "Tidal Testing",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Netus porttitor sem",
+    width: 160,
+    height: 100,
   },
 ];
 
 const Tracks = () => {
   return (
-    <div className="flex h-[120vh] flex-col items-center justify-start bg-[#4495B0]">
+    <div className="bg-hackathon-blue-300 flex h-[120vh] flex-col items-center justify-start px-16 py-16 md:w-full">
       <p className="flex pb-3 font-sora text-3xl font-bold text-white lg:mb-8 lg:text-5xl">
         TRACKS
       </p>
 
-      <div className="flex w-1/2 gap-4">
-        {tracks.map(({ image, title, description }, index) => (
+      <div className="grid gap-8 md:grid-cols-4">
+        {tracks.map(({ image, title, description, width, height }, index) => (
           <Track
             key={index}
             index={index}
             image={image}
             title={title}
             text={description}
+            width={width}
+            height={height}
           />
         ))}
       </div>
