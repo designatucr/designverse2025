3d61a69d0621fe53e2baae2662f96dd4dd57105c
py1u.dev <py1u.dev@gmail.com>
2025-01-23 18:49:40 -0800

    style: add padding between cards for smaller screens

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -41,12 +41,12 @@ const tracks = [
 
 const Tracks = () => {
   return (
-    <div className="bg-hackathon-blue-300 flex max-h-screen w-full flex-col items-center justify-center p-16">
-      <p className="flex pb-3 font-sora text-3xl font-bold text-white lg:mb-8 lg:text-5xl">
+    <div className="bg-hackathon-blue-300 sm: flex min-h-screen w-full flex-col items-center justify-center px-4 md:px-8">
+      <p className="flex py-8 font-sora text-3xl font-bold text-white lg:mb-8 lg:text-5xl">
         TRACKS
       </p>
 
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-4 sm:grid-cols-1 md:gap-8 lg:grid-cols-2 xl:grid-cols-4">
         {tracks.map(({ image, title, description, width, height }, index) => (
           <Track
             key={index}
