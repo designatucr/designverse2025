bba1fa6e793660e6d5838406c998af1d29f1fc7e
ACM UCR <fake@domain.com>
2025-01-30 04:00:38 +0000

    convert images to webp

--- src/components/live/judges.tsx
+++ src/components/live/judges.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Mock from "@/public/mock/mock_stanley.jpeg";
+import Mock from "@/public/mock/mock_stanley.webp";
 
 const judges = [
   {
