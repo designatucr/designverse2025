10e9366a3e3a544d680ac2f0ef0e698f2a2c828b
shahdivyank <divyank.shah.2016@gmail.com>
2025-02-23 18:12:53 -0800

    simplify

--- src/components/user/team/details.jsx
+++ src/components/user/team/details.jsx
@@ -163,18 +163,15 @@ const Details = ({ team }) => {
           {details.members.length === 0 &&
             "No Team Members. Invite others to join your team."}
 
-          {details.members.map((member, index) => (
+          {details.members.map(({ name, discord }, index) => (
             <div
               key={index}
               className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
             >
               <span className="flex h-2 w-2 translate-y-1 rounded-full bg-black" />
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {member.name} - {member.discord}
-                </p>
-                <p className="text-muted-foreground text-sm"></p>
-              </div>
+              <p className="text-sm font-medium leading-none">
+                {name} - {discord}
+              </p>
             </div>
           ))}
         </div>
