47287ca3eaefd445df0a2346869b905ca9385758
Andrew Khadder <akhad007@ucr.edu>
2025-03-04 14:49:07 -0800

    refactor layouts (fixes database bug)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 /* eslint-disable new-cap */
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Providers from "@/components/providers";
-import { Toaster } from "react-hot-toast";
-import { getServerSession } from "next-auth";
-import { options } from "@/utils/auth";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,17 +14,10 @@ type Props = {
 };
 
 const RootLayout = async ({ children }: Props) => {
-  const session = await getServerSession(options);
-
   return (
     <html lang="en" className="h-full">
       <body className={`${poppins.variable} flex h-full flex-col lg:flex-row`}>
-        <div className="flex h-full w-full">
-          <Providers session={session}>
-            <Toaster />
-            {children}
-          </Providers>
-        </div>
+        <div className="flex h-full w-full">{children}</div>
       </body>
     </html>
   );
---
+++ src/app/user/layout.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable new-cap */
+import Providers from "@/components/providers";
+import { Toaster } from "react-hot-toast";
+import { getServerSession } from "next-auth";
+import { options } from "@/utils/auth";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const UserLayout = async ({ children }: Props) => {
+  const session = await getServerSession(options);
+
+  return (
+    <Providers session={session}>
+      <Toaster />
+      {children}
+    </Providers>
+  );
+};
+
+export default UserLayout;
