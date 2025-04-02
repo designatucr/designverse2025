e1889a56ce66cd90d757fc498cc09a5b178767eb
Andrew Khadder <akhad007@ucr.edu>
2025-04-02 14:08:35 -0700

    add responsiveness to meet the team

--- src/components/live/team.tsx
+++ src/components/live/team.tsx
@@ -10,15 +10,15 @@ const Team = () => {
       id="team"
       className="flex flex-col items-center justify-start bg-[#3D859D]"
     >
-      <p className="mt-5 flex font-sora text-3xl font-bold text-white lg:mb-8 lg:mt-32 lg:text-5xl">
+      <p className="mt-8 flex font-sora text-3xl font-bold text-white lg:mb-8 lg:mt-32 lg:text-5xl">
         MEET THE TEAM
       </p>
 
-      <p className="mt-5 flex font-sora text-xl font-bold text-white lg:mb-8 lg:mt-0 lg:text-3xl">
+      <p className="my-8 flex font-sora text-2xl font-bold text-white lg:mb-8 lg:mt-0 lg:text-3xl">
         LEADS
       </p>
 
-      <div className="grid grid-cols-2 gap-16">
+      <div className="grid grid-cols-1 gap-16 lg:grid-cols-2">
         {directors.map(({ image, profile, name, position }, index) => (
           <Link
             href={profile}
@@ -39,7 +39,7 @@ const Team = () => {
         ))}
       </div>
 
-      <div className="my-16 grid grid-cols-5 gap-16">
+      <div className="my-16 grid grid-cols-1 gap-16 lg:grid-cols-5">
         {first.map(({ image, profile, name, position }, index) => (
           <Link
             href={profile}
@@ -60,7 +60,7 @@ const Team = () => {
         ))}
       </div>
 
-      <div className="grid grid-cols-4 gap-16">
+      <div className="grid grid-cols-1 gap-16 lg:grid-cols-4">
         {leads.map(({ image, profile, name, position }, index) => (
           <Link
             href={profile}
@@ -81,11 +81,11 @@ const Team = () => {
         ))}
       </div>
 
-      <p className="mt-5 flex font-sora text-xl font-bold text-white lg:mb-8 lg:mt-12 lg:text-3xl">
+      <p className="mb-3 mt-8 flex font-sora text-3xl font-bold text-white lg:mb-8 lg:mt-12 lg:text-3xl">
         COMMITTEE MEMBERS
       </p>
 
-      <div className="gap- grid grid-cols-3 gap-16 text-center text-white">
+      <div className="grid grid-cols-1 gap-16 text-center text-white md:grid-cols-3">
         {committees.map(({ team, members }, index) => (
           <div key={index}>
             <p className="text-2xl font-bold">{team}</p>
