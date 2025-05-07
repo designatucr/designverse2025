d288311e00fd4a2a97874a0dbbbc278223355e69
seanquiambao <paparoxii03@gmail.com>
2025-05-06 17:53:13 -0700

    build errors

--- src/app/judge/start/[id]/page.tsx
+++ src/app/judge/start/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Start from "@/components/judging/start";
-import ProtectedPage from "@/components/protected";
 
 interface props {
   params: {
@@ -12,14 +11,12 @@ const Page = ({ params, searchParams }: props) => {
   const { id } = params;
 
   return (
-    <ProtectedPage restrictions={{ judges: [1] }} title="Judge | Round">
-      <Start
-        id={id}
-        name={searchParams.name}
-        round={searchParams.round}
-        table={searchParams.table}
-      />
-    </ProtectedPage>
+    <Start
+      id={id}
+      name={searchParams.name}
+      round={searchParams.round}
+      table={searchParams.table}
+    />
   );
 };
 
--- src/app/judge/start/page.tsx
+++ src/app/judge/start/page.tsx
@@ -1,12 +1,8 @@
 import Dashboard from "@/components/judging/start/dashboard";
-import ProtectedPage from "@/components/protected";
 
 const Page = () => {
-  return (
-    <ProtectedPage restrictions={{ judges: [1] }} title="Judge | Start">
-      <Dashboard />
-    </ProtectedPage>
-  );
+  return;
+  <Dashboard />;
 };
 
 export default Page;
